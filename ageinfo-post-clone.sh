d1923faa4f111630093f2ecc0b749c23e4196c4f
Alexander Rojas <alexrrojas.b@gmail.com>
2021-09-11 22:25:10 -0600

    add post-clone script

---
+++ ageinfo-post-clone.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+ 
+# Install JSON tool
+echo "Install jq"
+brew install jq 
+ 
+# Install Cocoapods-keys
+echo "Installing gems"
+bundle install
+
+echo "Setting secrets"
+cd $APPCENTER_SOURCE_DIRECTORY
+bundle exec pod keys set "GoogleAdMobKey" "$GoogleAd" AgeInfo
+bundle exec pod keys set "RapidAPIKey" "$RapidApi" AgeInfo
+bundle exec pod keys set "AppCenterAnalitycs" "$AppCenter" AgeInfo
+
+echo "Installing pods"
+bundle exec pod install
+
+echo "Printing keys:"
+bundle exec pod keys
+
+echo "Generating secrets files"
+bundle exec pod keys generate AgeInfo
